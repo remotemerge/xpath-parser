0ef19a69d610b92fbcbc470b84d9cdf60b2150a8
Madan Sapkota <code@madansapkota.com>
2020-03-12 15:33:11 +0545

    Create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// init path module
+const path = require('path');
+
+module.exports = (env, argv) => ({
+  entry: {
+    'hali': './src/Hali.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'js/[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  watch: argv.mode !== 'production' || (argv.watch !== undefined && argv.watch === 'true'),
+  devtool: false,
+});
