176f3ef2dcfa4e1222b4b2a25bc7d1d6d9ffac84
Madan Sapkota <code@madansapkota.com>
2020-04-11 10:48:09 +0545

    Create test cases for multi results

---
+++ __tests__/query/multi-result.test.js
@@ -0,0 +1,20 @@
+import Hali from '../../dist/es';
+import productsHtml from '../products.html';
+
+const myHali = new Hali(productsHtml);
+const result = myHali.singleQuery('//span[contains(@class, "zg-item")]/a/div');
+
+// expect an object
+test('must return object of with titles', () => {
+  expect(typeof result).toBe('object');
+});
+
+// count the matches
+test('must have 50 titles', () => {
+  expect(result.length).toBe(50);
+})
+
+// expect title
+test('match the fist title', () => {
+  expect(result[0]).toBe('Cell Phone Stand,Angle Height Adjustable Stable LISEN Cell Phone Stand For Desk,Sturdy…');
+})
