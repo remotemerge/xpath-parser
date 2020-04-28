54954f9fe950a526df37e01abe6cac2794188951
Madan Sapkota <code@madansapkota.com>
2020-04-28 13:59:47 +0545

    Create test cases for wait feature

---
+++ __tests__/wait-selector.test.js
@@ -0,0 +1,19 @@
+import Hali from '../dist/es';
+import productsHtml from './data/products.html';
+
+// init hali
+const eHali = new Hali(productsHtml);
+
+test('must have an object with target', () => {
+  return eHali.waitXPath('//span[contains(@class, "a-color-price")]/span')
+    .then((response) => {
+      expect(response.found).toBe(true);
+    });
+});
+
+test('must have an object with error', () => {
+  return eHali.waitXPath('//span[contains(@class, "class-not-exists")]/span', 3)
+    .catch((error) => {
+      expect(error.message).toMatch('Timeout!');
+    });
+});
