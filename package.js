1a504da1da5a29b96b3aa5686c335791cea59b17
Madan Sapkota <contact@madansapkota.com>
2022-08-26 17:57:53 +0545

    Generate dynamic package config file

---
+++ package.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+
+// use vars from package config
+import pc from './package.json' assert { type: 'json' };
+
+// current directory
+const __dirname = path.resolve();
+
+// format configs
+const packageConfig = {
+  name: pc.name,
+  version: pc.version,
+  description: pc.description,
+  license: pc.license,
+  keywords: pc.keywords,
+  author: pc.author,
+  repository: pc.repository,
+  bugs: pc.bugs,
+  type: pc.type,
+  module: pc.module,
+};
+
+// generate chrome manifest
+const publicPath = `${__dirname}/public`;
+fs.existsSync(publicPath) || fs.mkdirSync(publicPath);
+fs.createWriteStream(`${publicPath}/package.json`, 'utf-8').write(JSON.stringify(packageConfig));
