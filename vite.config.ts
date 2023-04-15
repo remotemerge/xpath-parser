0c6bbd77fc4b070ac6d78c0cfaaffca4db0f35d4
Madan Sapkota <contact@madansapkota.com>
2023-04-15 07:59:23 +0545

    Generate multiple format

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// modules
 import { defineConfig } from 'vitest/config';
 
 // overwrite configs
@@ -7,16 +8,11 @@ export default defineConfig({
   },
   build: {
     minify: false,
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[hash].js',
-      },
-    },
     lib: {
-      entry: { 'xpath-parser': '/src/XPathParser.ts' },
-      formats: ['es'],
+      entry: 'src/index.ts',
+      name: 'XPathParser',
+      fileName: (format) => `index.${format}.js`,
     },
+    sourcemap: true,
   },
 });
